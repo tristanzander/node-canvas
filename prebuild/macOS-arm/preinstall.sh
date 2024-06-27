f255cd0267fe45b57291655c24a1e4932211a275
Tristan Zander <tristan.zander@ncino.com>
2024-06-27 12:31:15 -0400

    fixing macpack install

--- prebuild/macOS-arm/preinstall.sh
+++ prebuild/macOS-arm/preinstall.sh
@@ -1,3 +1,4 @@
 brew update
-brew install pkg-config cairo pango librsvg python3 python-setuptools giflib macpack # python3 is for macpack
+brew install pkg-config cairo pango librsvg python3 python-setuptools giflib # python3 is for macpack
 brew upgrade python # activates python 3
+pip3 install --user macpack
